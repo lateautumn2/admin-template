e132e9f17a78377f5e29c89686bb3d18818e4cbf
jiang jia le <jjl2801069764@gmail.com>
2022-09-30 17:37:31 +0800

    feat:axios封装优化

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -27,64 +27,66 @@ function createService() {
         ElMessage.error("非本系统的接口")
         return Promise.reject(new Error("非本系统的接口"))
       } else {
-        switch (code) {
-          case 0:
-            // code === 0 代表没有错误
-            return apiData
-          default:
-            // 不是正确的 Code
-            ElMessage.error(apiData.message || "Error")
-            return Promise.reject(new Error("Error"))
+        if (code !== 200) {
+          apiData.msg = switchErr(code, apiData.msg)
+          return Promise.reject(new Error(apiData.msg))
+        } else {
+          return apiData
         }
       }
     },
     (error) => {
       // Status 是 HTTP 状态码
       const status = get(error, "response.status")
-      switch (status) {
-        case 400:
-          error.message = "请求错误"
-          break
-        case 401:
-          // Token 过期时，直接退出登录并强制刷新页面（会重定向到登录页）
-          useUserStoreHook().logout()
-          location.reload()
-          break
-        case 403:
-          error.message = "拒绝访问"
-          break
-        case 404:
-          error.message = "请求地址出错"
-          break
-        case 408:
-          error.message = "请求超时"
-          break
-        case 500:
-          error.message = "服务器内部错误"
-          break
-        case 501:
-          error.message = "服务未实现"
-          break
-        case 502:
-          error.message = "网关错误"
-          break
-        case 503:
-          error.message = "服务不可用"
-          break
-        case 504:
-          error.message = "网关超时"
-          break
-        case 505:
-          error.message = "HTTP 版本不受支持"
-          break
-        default:
-          break
-      }
-      ElMessage.error(error.message)
+      error.msg = switchErr(status, error.msg)
       return Promise.reject(error)
     }
   )
   return service
+}
+
+const switchErr = (status: number, msg: string) => {
+  let message = "请求错误"
+  switch (status) {
+    case 401:
+      message = "登录过期，请重新登录！"
+      useUserStoreHook().logout()
+      location.replace("/login")
+      break
+    case 403:
+      message = "拒绝访问"
+      break
+    case 404:
+      message = "请求地址出错"
+      break
+    case 408:
+      message = "请求超时"
+      break
+    case 500:
+      message = "服务器内部错误"
+      break
+    case 501:
+      message = "服务未实现"
+      break
+    case 502:
+      message = "网关错误"
+      break
+    case 503:
+      message = "服务不可用"
+      break
+    case 504:
+      message = "网关超时"
+      break
+    case 505:
+      message = "HTTP 版本不受支持"
+      break
+    default:
+      break
+  }
+
+  ElMessage.error(msg ?? message)
+
+  return message
 }
 
 /** 创建请求方法 */
